7bb91253693440337354977babbf0486214d44be
kurtw555 <kurt.wolfe@gmail.com>
2018-02-28 18:00:21 -0500

    Make sure speciesid is a string before checking length

--- pisces_rest_api.py
+++ pisces_rest_api.py
@@ -84,7 +84,7 @@ def get_hucs_by_species(request, speciesid=''):
         e.g.
         https://qedinternal.epa.gov/pisces/rest/api/v1/hucs/fish/(speciesid)
     """
-
+    speciesid = str(speciesid)
     if len(speciesid) > 4:
         return JsonResponse({"error": "argument error: Species ID value provided was not valid, please provide a valid species ID."
                                       " Provided value = " + speciesid})
