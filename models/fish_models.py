caefe883b06c55000c0e065a88eb82f09b86d879
dbsmith88 <dbsmith@uga.edu>
2019-12-13 12:49:50 -0500

    update path

--- models/fish_models.py
+++ models/fish_models.py
@@ -4,7 +4,7 @@ import pandas
 import os
 
 
-species_data = pandas.read_csv(os.path.abspath('./pythonmodelstats.csv'), index_col=1, skiprows=0).T.to_dict()
+species_data = pandas.read_csv(os.path.abspath('./pisces_app/models/pythonmodelstats.csv'), index_col=1, skiprows=0).T.to_dict()
 
 
 class PiscesModel:
@@ -41,7 +41,7 @@ class PiscesModel:
 
     def run_model(self, inputs):
         model = XGBClassifier()
-        model_path = os.path.abspath('./Model_Files/Species_' + str(species_data[self.species_id]["Model"]))
+        model_path = os.path.abspath('./pisces_app/models/Model_Files/Species_' + str(species_data[self.species_id]["Model"]))
         model.load_model(model_path)
         pred = float(model.predict_proba(inputs)[:, 1])
         self.probability = round(100 * pred, 2)
